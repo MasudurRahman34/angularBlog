7fcfc98cb6031fe8b2ee501f7d4676624decadb9
MasudurRahman34 <mohammadmasud34@gmail.com>
2019-05-28 09:38:07 +0600

    routing complete without admin

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  RouterModule
+  ]
+})
+export class AppRoutingModule { }
